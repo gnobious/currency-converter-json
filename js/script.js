39c7ec943c0806208a4f1afd01d5994c1a4664a2
Roman <rimer93@gmail.com>
2021-06-26 20:44:28 +0300

    Updating the script and adding an external source

--- js/script.js
+++ js/script.js
@@ -1,57 +1,52 @@
-let inputUsd = document.getElementById('usd'),
-    inputEur = document.getElementById('eur'),
-    inputRub = document.getElementById('rub');
-    
-inputUsd.addEventListener('input', () => {
-    let request = new XMLHttpRequest();
-    request.open('GET', 'js/current.json');
-    request.setRequestHeader('Content-type', 'application/json; charset-utf-8');
-    request.send();
-    request.addEventListener('readystatechange', function() {
-        if (request.readyState ===4 && request.status == 200) {
-            let data = JSON.parse(request.response);
+window.addEventListener('DOMContentLoaded', function() {
+    'use strict'
 
-            inputRub.value = inputUsd.value * data.usdrub;
-            inputEur.value = inputUsd.value * data.usdeur;
-        } else {
+    let inputUsd = document.getElementById('usd'),
+        inputEur = document.getElementById('eur'),
+        inputRub = document.getElementById('rub'),
+        ratesSrc = 'https://www.floatrates.com/daily/rub.json'; //external file with the current exchange rate    
+   
+    //Connecting to the data source and calling the callback function
+    function grabData(source, callback) {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', source, true);
+        xhr.setRequestHeader('Content-type', 'application/json; charset-utf-8');
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                callback(xhr.response);
+            }          
+        };
+        xhr.send(null);
+    };
+
+    //The function parses the received data and converts
+    grabData(ratesSrc, function (data) {
+        let response = JSON.parse(data);
+        if (response) {
+            let rubeur = response.eur.inverseRate,
+                rubusd = response.usd.inverseRate,
+                usdrub = response.usd.rate,
+                eurrub = response.eur.rate;
+
+            inputUsd.addEventListener('input', () => {
+                inputRub.value = (inputUsd.value * rubusd).toFixed(3);
+                inputEur.value = (inputRub.value * eurrub).toFixed(3);
+            });
+
+            inputEur.addEventListener('input', () => {
+                inputRub.value = (inputEur.value * rubeur).toFixed(3);
+                inputUsd.value = (inputRub.value * usdrub).toFixed(3);            
+            });
+
+            inputRub.addEventListener('input', () => {
+                inputUsd.value = (inputRub.value * usdrub).toFixed(3);
+                inputEur.value = (inputRub.value * eurrub).toFixed(3);            
+            });
+        }
+        else if (isNaN(response) || response == "" || response == null) {
+            inputUsd.value = "Что-то пошло не так!";
             inputRub.value = "Что-то пошло не так!";
             inputEur.value = "Что-то пошло не так!";
         }
-    });
-});
-
- inputEur.addEventListener('input', () => {
-    let request = new XMLHttpRequest();
-    request.open('GET', 'js/current.json');
-    request.setRequestHeader('Content-type', 'application/json; charset-utf-8');
-    request.send();
-    request.addEventListener('readystatechange', function() {
-        if (request.readyState ===4 && request.status == 200) {
-            let data = JSON.parse(request.response);
-
-            inputRub.value = inputEur.value * data.eurrub;
-            inputUsd.value = inputEur.value * data.eurusd;
-        } else {
-            inputRub.value = "Что-то пошло не так!";            
-            inputUsd.value = "Что-то пошло не так!";
-        }
-    });
-});
-
-inputRub.addEventListener('input', () => {
-    let request = new XMLHttpRequest();
-    request.open('GET', 'js/current.json');
-    request.setRequestHeader('Content-type', 'application/json; charset-utf-8');
-    request.send();
-    request.addEventListener('readystatechange', function() {
-        if (request.readyState ===4 && request.status == 200) {
-            let data = JSON.parse(request.response);
-
-            inputUsd.value = inputRub.value / data.usdrub;
-            inputEur.value = inputRub.value / data.eurrub;
-        } else {
-            inputUsd.value = "Что-то пошло не так!";
-            inputEur.value = "Что-то пошло не так!";
-        }
-    });
-});
+    });         
+});    
