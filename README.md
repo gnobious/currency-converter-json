dfaa04cd65208dbbcbd2d5320768d17ac0b17955
Roman <rimer93@gmail.com>
2021-07-05 16:50:06 +0300

    New currencies, Promises, design and adaptation
    - Added 3 new currencies;
    - The script has been optimized to work with a large number of currencies and the use of promises has been added;
    - The design and svg logo for the project were also created;
    - Adaptation for all types of devices has been added;
    - Updated documentation;
    - Added .gitignore

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Currency converter using JSON and XHR
 
-A JavaScript application for currency conversion using external JSON.
+A JavaScript application for currency conversion using external JSON, Promises. Also adapted for all devices.
 
 ## Experience 🎓
 
@@ -8,7 +8,9 @@ In  this  project i practice:
 
 * Using **JSON** format;
 * Connecting by **XMLHttpRequest**;
-* Using a callback for sequential execution of functions.
+* Using **Promises**;
+* Using a callback for sequential execution of functions;
+* Creating design and *SVG* logo.
 
 ## Demo 🎥
 
@@ -19,3 +21,16 @@ The completed project can be viewed [here](https://gnobious.github.io/currency-c
 ***Running a project locally***
 
 If you wish to run project locally as is, then all that's required is the `style.css`, `index.html` files and `js` folder from this repository. Then just need to open `index.html`.
+
+## Design and Logo 🎨
+
+For the project, colors were chosen that are combined with each other and convey the financial bias of the site
+
+* ![#777777](https://via.placeholder.com/15/777777/000000?text=+) `#777777`
+* ![#16b67f](https://via.placeholder.com/15/16b67f/000000?text=+) `#16b67f`
+* ![#2dcc70](https://via.placeholder.com/15/2dcc70/000000?text=+) `#2dcc70`
+* ![#0d6948](https://via.placeholder.com/15/0d6948/000000?text=+) `#0d6948`
+
+### *Logo*
+
+<img src="img/logo.svg" alt="SVG logo demonstration" width=320px hspace=250>
--- js/script.js
+++ js/script.js
@@ -1,52 +1,61 @@
 window.addEventListener('DOMContentLoaded', function() {
     'use strict'
 
-    let inputUsd = document.getElementById('usd'),
-        inputEur = document.getElementById('eur'),
-        inputRub = document.getElementById('rub'),
+    let inputStack = document.querySelectorAll('input'),
         ratesSrc = 'https://www.floatrates.com/daily/rub.json'; //external file with the current exchange rate    
    
-    //Connecting to the data source and calling the callback function
-    function grabData(source, callback) {
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', source, true);
-        xhr.setRequestHeader('Content-type', 'application/json; charset-utf-8');
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-                callback(xhr.response);
-            }          
-        };
-        xhr.send(null);
-    };
+    inputStack.forEach(item => {item.addEventListener('focus', () => {item.value = '';})}); //Clearing the input when focusing
+
+    //Connecting to the data source and creating Promise
+    function grabData(source) {
+        return new Promise(function(resolve,reject){
+            let xhr = new XMLHttpRequest();
+            xhr.open('GET', source);
+            xhr.setRequestHeader('Content-type', 'application/json; charset-utf-8');
+            xhr.onload = function() {
+                if (this.status >= 200 && this.status < 300) {
+                    resolve(xhr.response);
+                } else {
+                    reject({
+                        status: this.status,
+                        statusText: xhr.statusText
+                    });
+                }         
+            };
+            xhr.onerror = function () {
+                reject({
+                    status: this.status,
+                    statusText: xhr.statusText
+                });
+            };
+            xhr.send();
+        });        
+    }
 
     //The function parses the received data and converts
-    grabData(ratesSrc, function (data) {
-        let response = JSON.parse(data);
-        if (response) {
-            let rubeur = response.eur.inverseRate,
-                rubusd = response.usd.inverseRate,
-                usdrub = response.usd.rate,
-                eurrub = response.eur.rate;
-
-            inputUsd.addEventListener('input', () => {
-                inputRub.value = (inputUsd.value * rubusd).toFixed(3);
-                inputEur.value = (inputRub.value * eurrub).toFixed(3);
-            });
-
-            inputEur.addEventListener('input', () => {
-                inputRub.value = (inputEur.value * rubeur).toFixed(3);
-                inputUsd.value = (inputRub.value * usdrub).toFixed(3);            
-            });
-
-            inputRub.addEventListener('input', () => {
-                inputUsd.value = (inputRub.value * usdrub).toFixed(3);
-                inputEur.value = (inputRub.value * eurrub).toFixed(3);            
-            });
-        }
-        else if (isNaN(response) || response == "" || response == null) {
-            inputUsd.value = "Что-то пошло не так!";
-            inputRub.value = "Что-то пошло не так!";
-            inputEur.value = "Что-то пошло не так!";
-        }
-    });         
-});    
+    grabData(ratesSrc)        
+        .then(data => {                           
+            let parsedData = JSON.parse(data);
+            inputStack.forEach((listenedItem) => {listenedItem.addEventListener('input', () => {
+                let currentEl = listenedItem.id,
+                    ruble;
+                if (currentEl == 'rub') {           //  Since the reference currency in our json file is the ruble,
+                    ruble = listenedItem.value;    //  all conditions and calculations are based on this factor
+                } else {
+                    ruble = listenedItem.value/parsedData[currentEl].rate
+                };
+                inputStack.forEach((item) => {                    
+                    if (item.id !== currentEl && item.id !== 'rub') {
+                        item.value = (parsedData[item.id].rate * ruble).toFixed(3);
+                    } else if (item.id !== currentEl && item.id == 'rub') {
+                        item.value = ruble.toFixed(3);
+                    }
+                })                
+            })});  
+        })       
+        .catch((error) => {
+            console.log(`Сервер выдал ошибку: Статус - ${error.code};
+            Текст: ${error}`)
+            inputStack.forEach((item) => {item.value = `Ошибка (код ${error.code})`});
+        })
+});
